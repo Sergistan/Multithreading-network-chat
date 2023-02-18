1ce4bddec9f6112b2c45af5b1dcd744dd35b30ee
Sergei Utochkin <dzaga73i98@gmail.com>
2023-02-18 16:17:30 +0300

    first ver chat (multithreading)

--- Server/src/main/java/ChatServer/ChatServer.java
+++ Server/src/main/java/ChatServer/ChatServer.java
@@ -5,10 +5,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class ChatServer {
 
     private static int PORT;
+    private static ArrayList<Thread> threads = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("Server/src/main/resources/settings.txt"))) {
@@ -27,29 +29,58 @@ public class ChatServer {
                  PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
                  BufferedWriter outLog = new BufferedWriter(new FileWriter("File.log", true))) {
 
-                out.write("Перед тем как начать общаться напишите ваше имя!" + "\n");
-                out.flush();
+                Thread thread = new Thread(()->{
+                    out.write("Перед тем как начать общаться напишите ваше имя!" + "\n");  ////////////////////////////////////////
+                    out.flush();
 
-                String name = in.readLine();
-                System.out.println("В чат присоединился: " + name);
-                outLog.write("В чат присоединился: " + name + " " +  LocalDateTime.now() + "\n");
-
-                while (!clientSocket.isClosed()) {
-
-                    String massageFromUser = in.readLine();
-                    if (!massageFromUser.isEmpty()) {
-                        if (massageFromUser.equals("/exit")) {
-                            System.out.println("Client initialize connections suicide ...");
-                            out.write("Server reply - " + massageFromUser + " - OK");
-                            out.flush();
-                            break;
-                        }
-                        outLog.write(name + ": " +  " " + massageFromUser + " " + LocalDateTime.now() + "\n");
-                        outLog.flush();
+                    String name = null;
+                    try {
+                        name = in.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+                    System.out.println("В чат присоединился: " + name);
+                    try {
+                        outLog.write("В чат присоединился: " + name + " " +  LocalDateTime.now() + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    while (!clientSocket.isClosed()) {
+                        String massageFromUser = null;
+                        try {
+                            massageFromUser = in.readLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (!massageFromUser.isEmpty()) {
+                            if (massageFromUser.equals("/exit")) {
+                                System.out.println("Client initialize connections suicide ...");
+                                out.write("Server reply - " + massageFromUser + " - OK");
+                                out.flush();
+                                break;
+                            }
+                            try {
+                                outLog.write(name + ": " +  " " + massageFromUser + " " + LocalDateTime.now() + "\n");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                outLog.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }                                                                           ///////////////////////////////////////////////
+                });
+                threads.add(thread);
+
+                for (Thread t: threads) {
+                    t.join();
                 }
 
-            } catch (IOException e) {
+
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
 
